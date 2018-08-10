b82382d52de5b7f4f3a8f30a9d960656a112bca9
HussnainJaffri <hajaffri33@gmail.com>
2018-08-10 11:10:11 -0700

    Here you go

--- app/src/main/java/com/hextogen/daurm/Splash.java
+++ app/src/main/java/com/hextogen/daurm/Splash.java
@@ -1,14 +1,34 @@
 package com.hextogen.daurm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Splash extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        Thread mythread = new Thread(){
+
+
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    startActivity(new Intent(Splash.this, MainActivity.class));
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        mythread.start();
+
+
     }
 }
-
