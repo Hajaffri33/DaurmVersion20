5164431b74dd9141576f091ea670719c93815f30
HussnainJaffri <hajaffri33@gmail.com>
2018-08-12 18:14:45 -0700

    latest

--- app/src/main/java/com/hextogen/daurm/teacher_panel.java
+++ app/src/main/java/com/hextogen/daurm/teacher_panel.java
@@ -15,8 +15,7 @@ public class teacher_panel extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
     Button signout;
-    Button time_table;
-    Button ongoing_session;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,15 +23,6 @@ public class teacher_panel extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         signout = findViewById(R.id.panel_signout);
-        time_table=findViewById(R.id.time_table);
-        ongoing_session=findViewById(R.id.ongoing_session);
-        time_table.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(teacher_panel.this,teacher_timetable.class));
-
-            }
-        });
 
 
         signout.setOnClickListener(new View.OnClickListener() {
@@ -46,43 +36,29 @@ public class teacher_panel extends AppCompatActivity {
 
             }
         });
-        ongoing_session.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(teacher_panel.this,ongoing.class ));
-            }
-        });
     }
-
-
-
 
 
     public void gotoLock(View view) {
 
 
-
-        FirebaseUser user =FirebaseAuth.getInstance().getCurrentUser();
-        String mail1  = "asifkhan@uogsialkot.edu.pk" ;
-        String mail2  = "ad@uogsialkot.edu.pk" ;
-        String mail3  = "faizanarshad@uogsialkot.edu.pk" ;
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String mail1 = "asifkhan@uogsialkot.edu.pk";
+        String mail2 = "ad@uogsialkot.edu.pk";
+        String mail3 = "faizanarshad@uogsialkot.edu.pk";
         String email = user.getEmail();
 
 
-
-        if(email.equals(mail1)){
+        if (email.equals(mail1)) {
 
             startActivity(new Intent(teacher_panel.this, door1.class));
-        }
-        else if(email.equals(mail2)){
+        } else if (email.equals(mail2)) {
 
             startActivity(new Intent(teacher_panel.this, door2.class));
-        }
-        else if(email.equals(mail3)){
+        } else if (email.equals(mail3)) {
 
             startActivity(new Intent(teacher_panel.this, door3.class));
-        }
-        else{
+        } else {
 
             Toast.makeText(this, "Not your class", Toast.LENGTH_SHORT).show();
         }
